f084cc659b34b4a0f0268dbfe1b93ac98e6d4dee
Jeremy <jeremy@foxtrot.io>
2017-07-07 11:25:19 -0700

    WIP 6 remove sed statements from kafka start script

--- kafka/scripts/start-kafka.sh
+++ kafka/scripts/start-kafka.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 
 echo "broker id: ${BROKER_ID}"
-sed -r -i "s/<<<BROKER_ID>>>/${BROKER_ID}/" ${KAFKA_HOME}/config/server.properties
+#sed -r -i "s/<<<BROKER_ID>>>/${BROKER_ID}/" ${KAFKA_HOME}/config/server.properties
 echo "zk hosts: ${ZK_HOSTS}"
-sed -r -i "s/<<<ZK_HOSTS>>>/${ZK_HOSTS}/" ${KAFKA_HOME}/config/server.properties
+#sed -r -i "s/<<<ZK_HOSTS>>>/${ZK_HOSTS}/" ${KAFKA_HOME}/config/server.properties
 echo "advertised hosts: ${ADVERTISED_HOST}"
-sed -r -i "s/<<<ADVERTISED_HOST>>>/${ADVERTISED_HOST}/g" ${KAFKA_HOME}/config/server.properties
+#sed -r -i "s/<<<ADVERTISED_HOST>>>/${ADVERTISED_HOST}/g" ${KAFKA_HOME}/config/server.properties
 
 # Run Kafka
 ${KAFKA_HOME}/bin/kafka-server-start.sh ${KAFKA_HOME}/config/server.properties
